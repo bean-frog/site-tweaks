43a05e101aff36e08ee8f68f6dc60258be4a77c9
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-11-08 15:00:12 -0800

    Update and rename schoology/schoology-addSlopeGame.js to pausd.schoology.com/schoology-addSlopeGame.js

--- pausd.schoology.com/schoology-addSlopeGame.js
+++ pausd.schoology.com/schoology-addSlopeGame.js
@@ -3,7 +3,7 @@
 // @version      0.1
 // @description  adds the slope game to your schoology homepage
 // @author       graeme kieran
-// @match        pausd.schoology.com/home
+// @match        pausd.schoology.com/*
 // @icon         https://cdn-icons-png.flaticon.com/512/3422/3422228.png
 // @grant        none
 // ==/UserScript==
