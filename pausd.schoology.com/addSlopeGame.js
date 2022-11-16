0a098452b33742f448d95473b76acfff95fd40e2
bean-frog <104943861+bean-frog@users.noreply.github.com>
2022-11-16 13:54:55 -0800

    Update addSlopeGame.js

--- pausd.schoology.com/addSlopeGame.js
+++ pausd.schoology.com/addSlopeGame.js
@@ -13,7 +13,7 @@
 //toggle visibility using jquery
     let togbtn = document.createElement('button');
     togbtn.setAttribute('id', 'togbtn');
-    togbtn.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    togbtn.textContent = 'toggle';
     document.body.appendChild(togbtn);
 
 
