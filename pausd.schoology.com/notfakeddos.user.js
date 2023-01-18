f36320911504c1bded904671ba15d4d1e661ee44
bean-frog <104943861+bean-frog@users.noreply.github.com>
2023-01-17 18:36:25 -0800

    Create notfakeddos.user.js

---
+++ pausd.schoology.com/notfakeddos.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name       not fake ddos
+// @namespace  http://gkieran.github.io/
+// @version    0.1
+// @description  Replace the entire body of a web page with new HTML
+// @match      https://pausd.schoology.com/*
+// @grant      none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let newHTML = `
+<table width="100%" height="100%" cellpadding="30">
+<tr>
+
+
+<div id="challenge" style="width: 100%; margin: 0 auto; text-align: center;">
+<h1 style="display: block; font-size: 32px">Checking your browser before accessing pausd.schoology.com.</h1>
+<p style="font-size: 18px; margin-top: 50px">This process is automatic. Your browser will redirect to your requested content shortly.</p>
+<p style="font-size: 12px">Please allow up to 5 seconds...</p>
+<img src="https://i.imgur.com/t6DVUDi.gif" style="margin: 40px 0 30px 0;"/>
+</div>
+<div style="margin-top:20px; width: 100%; margin: 0 auto; text-align: center;">
+<a href="http://www.cloudflare.com/" target="_blank" style="font-size: 12px;">DDoS protection by CloudFlare</a>
+</div>
+</td>
+</tr>
+</table>
+    `
+
+    document.body.innerHTML = newHTML;
+    document.title = "Just a moment...";
+
+})();
